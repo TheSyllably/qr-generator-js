f0d75a7cf6ad459e4dc5cdc2e465031677030cbe
TheSyllably <goldwynmayerinc@inbox.ru>
2023-10-06 01:10:23 +0400

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [`auto`],
+    theme: {
+          extend: {
+            fontFamily: {
+              sans: ['Poppins', 'sans-serif'],
+            },
+          },
+        },
+  }
